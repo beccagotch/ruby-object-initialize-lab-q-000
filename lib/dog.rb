4be63b5e8f7efa2e708e90771f750d10660924dd
beccagotch <becca.gotch@archiv8.com>
2016-05-08 22:08:42 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+ #get
+  def initialize(name, breed="Mut")
+    @name = name
+    @breed = breed
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+end
+
+
+#Dog.new("Mut")
+
+#end
+end
